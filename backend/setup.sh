a7e290deab35d0d1a4503d28324d1b8c49a5c2d6
Raethlein, Benjamin <benjamin.raethlein@sap.com>
2016-10-22 01:22:17 +0200

    Added title to sample image

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,5 +1,6 @@
 brew install mongodb
 mongod --fork --dbpath ./mongo/data --logpath ./mongo/logs/mongo.logs
+# mongoexport --db images --collection image --out sample.json
 mongoimport --db images --collection image --file ./sample.json
 
 npm install express
