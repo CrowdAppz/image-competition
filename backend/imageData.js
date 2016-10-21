2d5615031c8e6268dc5ff9f85bc956dfd4d90b66
Raethlein, Benjamin <benjamin.raethlein@sap.com>
2016-10-21 23:26:29 +0200

    Added some sample data

--- backend/imageData.js
+++ backend/imageData.js
@@ -1,15 +1,17 @@
 class Image {
   constructor(title){
-    this.imageBase64;
     this.title = title;
-    this.comments;
+    this.comments = [];
+    this.tags = [];
+    this.imageBase64;
   }
 
   toJson(){
     return {
       'title': this.title,
+      'comments': this.comments,
+      'tags': this.tags,
       'imageBase64': this.imageBase64,
-      'comments': this.comments
     }
   }
 }
--- backend/setup.sh
+++ backend/setup.sh
@@ -1,5 +1,6 @@
 brew install mongodb
 mongod --fork --dbpath ./mongo/data --logpath ./mongo/logs/mongo.logs
+mongoimport --db images --collection image --file ./sample.json
 
 npm install express
 npm install body-parser
