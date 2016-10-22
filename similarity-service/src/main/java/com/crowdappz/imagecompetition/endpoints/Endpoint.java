890ae0e157a5ebc1353020dfb2b2d16957437ab7
Raethlein, Benjamin <benjamin.raethlein@sap.com>
2016-10-22 19:58:56 +0200

    Possibility to return similar words for multiple words

--- similarity-service/src/main/java/com/crowdappz/imagecompetition/endpoints/Endpoint.java
+++ similarity-service/src/main/java/com/crowdappz/imagecompetition/endpoints/Endpoint.java
@@ -2,16 +2,12 @@ package com.crowdappz.imagecompetition.endpoints;
 
 import com.crowdappz.imagecompetition.WordVectorHandler;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by d056954 on 22/10/2016.
@@ -25,5 +21,19 @@ public class Endpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getSimilarWords(@PathParam("word") final String word, @PathParam("limit") final int limit){
         return Response.ok(new GenericEntity<List<String>>(WordVectorHandler.getNearestWords(word, limit)){}).build();
+    }
+
+    @GET
+    @Path("/words/similar/{limit}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSimilarWordsFromWordBag(@QueryParam("words") String words, @PathParam("limit") final int limit){
+        String[] splitWords = words.split(",");
+        Set<String> allNearestWords = new HashSet<String>();
+        for(String word : splitWords){
+            List<String> nearestWords = WordVectorHandler.getNearestWords(word, limit);
+            allNearestWords.addAll(nearestWords);
+        }
+
+        return Response.ok(new GenericEntity<Set<String>>(allNearestWords){}).build();
     }
 }
